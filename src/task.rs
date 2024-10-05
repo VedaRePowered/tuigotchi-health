463190bd860f6f6cd9669d77419b6eb8988b9100
Morgan Hager <goose121@users.noreply.github.com>
2024-10-05 11:23:45 -0600

    Wrap license notices

--- src/task.rs
+++ src/task.rs
@@ -1,15 +1,19 @@
 /*
 This file is part of Tamagotchi Health.
 
-Tamagotchi Health is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
-later version.
+Tamagotchi Health is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-Tamagotchi Health is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+Tamagotchi Health is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Tamagotchi Health. If not, see
-<https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+// along with Tamagotchi Health. If not, see
+// <https://www.gnu.org/licenses/>.
 */
 
 pub struct Task {
