484dd679395e3afbcbd9c3fc2d34af8739555232
Veda <veda@vedapowered.ca>
2024-10-05 10:53:14 -0600

    Initiaw Cowwit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Tamagotchi Health
+
+This is a command line tui self-care pet application!
+
+uwu yay!!!!
+
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+/*
+This file is part of Tamagotchi Health.
+
+Tamagotchi Health is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+Tamagotchi Health is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Tamagotchi Health. If not, see
+<https://www.gnu.org/licenses/>. 
+*/
+
+fn main() {
+    println!("\x1b[35muwu!\x1b[0m");
+}
